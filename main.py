0012c153ac30d854720324e2ac423f33005890f6
ALi odeh <109085886+3liodeh@users.noreply.github.com>
2023-06-01 20:18:16 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from bardapi import Bard
+import os
+
+os.environ['_BARD_API_KEY']="XAhr4hmTK4v2pnZB4rzBvSjAeDSMOF5DtqO42EfHGYOQL2i66RcFQJD-eh9SknrVElY-iw."
+
+
+def appearance():
+    dark_theme = """
+        <style>
+        .stApp {
+        background-color: #1F2227;
+        background-image: linear-gradient(to bottom right, #33FFE9, #2E8B57);
+        background-size: cover;
+        
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        text-align: center;}
+                                          
+       .stTextInput label {
+        display: flex;
+        justify-content: center;
+        border: none;
+        box-shadow: none;
+        }
+        
+        [data-testid="stHeader"]{
+            background-color: #000000;
+            }
+    .css-pxxe24 {
+    visibility: hidden;
+    }
+        </style>
+        """
+    st.markdown(dark_theme, unsafe_allow_html=True)
+
+
+appearance()
+
+st.title('Ask Bard')
+
+# Create a box container
+with st.container():
+    # Add content inside the box
+    text = st.text_input('Enter your text')
+    click=st.button("ASK")
+    if click:
+        st.write('The asnwer :', Bard().get_answer(str(text))['content'])
+
+
